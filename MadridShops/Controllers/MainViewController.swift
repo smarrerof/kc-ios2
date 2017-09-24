bb56caf40671cb22fb11b8b5e9ffd6db4c6ba08b
Sergio Marrero <smarrerof@gmail.com>
2017-09-24 13:24:48 +0100

    Checked connectivity when app is launched

--- MadridShops/Controllers/MainViewController.swift
+++ MadridShops/Controllers/MainViewController.swift
@@ -19,10 +19,16 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ExecuteOnceInteractorImpl().execute(onceClosure: {
-            initializeShopsData()
-        }) {
-            updateUI()
+        if isConnectedToNetwork() {
+            ExecuteOnceInteractorImpl().execute(onceClosure: {
+                initializeShopsData()
+            }) {
+                updateUI()
+            }
+        }
+        else {
+            showAlert()
+            loadingActivityIndicatorView.stopAnimating()
         }
     }
     
@@ -40,6 +46,15 @@ class MainViewController: UIViewController {
     func updateUI() {
         loadingActivityIndicatorView.stopAnimating()
         shopsButton.isEnabled = true
+    }
+    
+    func showAlert() {
+        let alertController = UIAlertController(title: "Ups, algo fue mal", message:
+            "Parece que no hay conectividad a internet. Cierra la aplicación y vuelve a lanzarla cuando se hay solucionado el problema. ", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alertController.addAction(UIAlertAction(title: "Accept", style: UIAlertActionStyle.default, handler: nil))
+        
+        self.present(alertController, animated: true, completion: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
